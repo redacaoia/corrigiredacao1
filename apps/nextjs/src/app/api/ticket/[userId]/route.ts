c69d4f9409e5e42c1aa32a3f5ae0a33bb1f3a1fc
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-01-23 16:52:15 +0000

    new

--- apps/nextjs/src/app/api/ticket/[userId]/route.ts
+++ apps/nextjs/src/app/api/ticket/[userId]/route.ts
@@ -1,3 +1,27 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export async function GET() {
+  return new ImageResponse((<div
+    style={{
+      fontSize: 128,
+      background: 'white',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    About Acme
+  </div>),
+  {
+    width: 1200,
+    height: 600,
+  });
+}
+
 /* import { NextRequest } from "next/server";
 import { SAMPLE_TICKET_NUMBER, SITE_URL } from "@/lib/constants";
 import { getUserByUsername } from "@/lib/getUserByUsername";
