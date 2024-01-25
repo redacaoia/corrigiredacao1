8e59e042a0cc3d06772b6de41c885106fe336600
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-01-25 22:47:29 +0000

    new

---
+++ apps/nextjs/src/app/(app)/temas/[correctionId]/page.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default async function Correction() {
+    return (
+<h1>Helo world</h1>
+    )
+}
---
+++ apps/nextjs/src/app/(app)/temas/layout.tsx
@@ -0,0 +1,27 @@
+import { IoClose } from "react-icons/io5";
+
+/* import { Header } from "@/components/app/Header";
+import { Footer } from "@/components/Footer";
+ */
+interface Props {
+  children: React.ReactNode;
+}
+
+export default async function TemasLayout({ children }: Props) {
+  return (
+    <>
+      <main className="relative">
+        <div
+          className="fixed top-0  flex w-full items-start justify-between  rounded-b-lg bg-lime-500
+      transition-all "
+        >
+          <div className="flex gap-2">
+            <IoClose />
+            <h2>Corrigir Redação</h2>
+          </div>
+        </div>
+        {children}
+      </main>
+    </>
+  );
+}
