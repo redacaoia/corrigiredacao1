af04a08fc9eef44363fa7f2d65ada8c56a0b80c3
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-02-02 19:58:15 -0300

    new

--- apps/nextjs/src/app/head.tsx
+++ apps/nextjs/src/app/head.tsx
@@ -32,6 +32,16 @@ export default function Head() {
         name="google-site-verification"
         content="-k1G8EWDVpOxlkTyRyg4dzfSTXR3if8c7QSeAHodbJ4"
       />
+
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P5KF66G2')`,
+        }}
+      />
     </>
   );
 }
