b7cfdab07d167d09580c4b0f4a61193db12fa0db
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-02-06 12:16:17 +0000

    new

--- apps/auth-proxy/routes/[...auth].ts
+++ apps/auth-proxy/routes/[...auth].ts
@@ -1,5 +1,8 @@
 import { Auth } from "@auth/core";
 import Discord from "@auth/core/providers/discord";
+import Facebook from "@auth/core/providers/facebook";
+import Google from "@auth/core/providers/google";
+import Instagram from "@auth/core/providers/instagram";
 import { eventHandler, toWebRequest } from "h3";
 
 export default eventHandler(async (event) =>
@@ -12,6 +15,9 @@ export default eventHandler(async (event) =>
         clientId: process.env.AUTH_DISCORD_ID,
         clientSecret: process.env.AUTH_DISCORD_SECRET,
       }),
+      Google({ clientId: process.env.AUTH_GOOGLE_CLIENT_ID, clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET }),
+      Instagram({ clientId: process.env.AUTH_INSTAGRAM_CLIENT_ID, clientSecret: process.env.AUTH_INSTAGRAM_CLIENT_SECRET }),
+      Facebook({ clientId: process.env.AUTH_FACEBOOK_CLIENT_ID, clientSecret: process.env.AUTH_FACEBOOK_CLIENT_SECRET })
     ],
   }),
 );
