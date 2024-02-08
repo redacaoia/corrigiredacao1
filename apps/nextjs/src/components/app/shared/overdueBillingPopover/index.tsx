f5fb51b5699e746d29715e223dac4d121ee5d696
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-02-08 18:54:58 +0000

    new

--- apps/nextjs/src/components/app/shared/overdueBillingPopover/index.tsx
+++ apps/nextjs/src/components/app/shared/overdueBillingPopover/index.tsx
@@ -1,4 +1,3 @@
-
 import Image from "next/image";
 import {
   AlertDialog,
@@ -25,20 +24,27 @@ export function OverdueBilling() {
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>
-            Você está prestes a <strong className="underline-offset-2">perder</strong> os benefícios!
+            Você está prestes a{" "}
+            <strong className="underline-offset-2">perder</strong> os
+            benefícios!
           </AlertDialogTitle>
           <AlertDialogDescription>
             <strong>Não deixe sua jornada para o sucesso estagnar.</strong>
             Sem a renovação da sua assinatura,você poderá não apenas o acesso
             mas tambem tudo qu contruiu aqui!
             <div className="flex items-center gap-2 bg-gradient-to-br  from-indigo-50 via-white to-cyan-100 py-32">
-              <Image src="/emojis/sad-face.png" alt="sad-face" width={24}  height={24}/>
+              <Image
+                src="/emojis/sad-face.png"
+                alt="sad-face"
+                width={24}
+                height={24}
+              />
               <div>
                 <h2 className="font-bold">sua assinatura venceu ontem</h2>
                 <span>01/06/23</span>
               </div>
               <div className="text-red-600/100">
-                <span className="font-extrabold text-5xl">6</span>
+                <span className="text-5xl font-extrabold">6</span>
                 <div>
                   <h2 className="font-bold">dias restantes para encerrar</h2>
                   <span>06/06/2023</span>
