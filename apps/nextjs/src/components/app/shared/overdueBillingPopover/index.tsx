e37433e749642e20c02b554d69a65aba84f18ec2
WalysonMoura <walysonmoura222@gmail.com>
2024-02-08 20:36:30 -0300

    new

--- apps/nextjs/src/components/app/shared/overdueBillingPopover/index.tsx
+++ apps/nextjs/src/components/app/shared/overdueBillingPopover/index.tsx
@@ -23,8 +23,8 @@ export function OverdueBilling() {
 
       <AlertDialogContent>
         <AlertDialogHeader>
-          <AlertDialogTitle>
-            Você está prestes a 
+          <AlertDialogTitle className="text-xl font-semibold">
+            Você está prestes a
             <strong className="underline-offset-2">perder</strong> os
             benefícios!
           </AlertDialogTitle>
@@ -32,8 +32,8 @@ export function OverdueBilling() {
             <strong>Não deixe sua jornada para o sucesso estagnar.</strong>
             Sem a renovação da sua assinatura,você poderá não apenas o acesso
             mas tambem tudo qu contruiu aqui!
-            <div className="flex items-center gap-8  bg-cyan-100  p-5">
-              {/* bg-gradient-to-br  from-indigo-50 via-white to-cyan-100 */}
+            <div className="flex items-center gap-8  bg-gradient-to-br  from-indigo-50 via-cyan-100 to-indigo-50  p-5">
+              {/* bg-gradient-to-br  from-indigo-50 via-cyan-100 to-indigo-50 */}
               <Image
                 src="/emojis/sad-face.png"
                 alt="sad-face"
@@ -45,7 +45,7 @@ export function OverdueBilling() {
                 <span>01/06/23</span>
               </div>
               <div className="flex items-center gap-4 text-red-600/100 ">
-                <span className="text-9xl font-extrabold">6</span>
+                <span className="text-7xl font-extrabold">2</span>
                 <div className="flex flex-col gap-2">
                   <h2 className="font-bold">dias restantes para encerrar</h2>
                   <span>06/06/2023</span>
@@ -59,7 +59,7 @@ export function OverdueBilling() {
             <AlertDialogAction className="h-11 rounded-full px-8">
               Renovar assinatura
             </AlertDialogAction>
-            <AlertDialogCancel className="border-transparent rounded-full">
+            <AlertDialogCancel className="rounded-full border-transparent">
               Decidir depois
             </AlertDialogCancel>
           </div>
