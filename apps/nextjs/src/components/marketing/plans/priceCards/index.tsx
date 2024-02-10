a7959f0b35fedc84b3a439bf00a37eba2587fc63
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-02-10 20:50:53 +0000

    new

--- apps/nextjs/src/components/marketing/plans/priceCards/index.tsx
+++ apps/nextjs/src/components/marketing/plans/priceCards/index.tsx
@@ -7,15 +7,15 @@ import { Separator } from "~/components/ui/separator";
 
 export function PriceCards() {
   return (
-    <section>
-      <Card className="relative flex flex-col gap-8 p-6 pt-10">
-        <span className="absolute -top-4 left-4 flex items-center gap-4 rounded-lg bg-blue1 p-2 font-medium text-gray-100">
+    <section className="grid grid-cols-1 gap-24 md:grid-cols-3 md:gap-6">
+      <Card className="relative flex flex-col justify-between gap-12 bg-primary p-6 pt-10 text-gray-100 shadow-2xl">
+        <span className="absolute -top-4 left-4 flex items-center gap-2 rounded-sm bg-blue1 p-2 font-medium text-gray-100">
           Melhor Plano <MdStars size={24} />
         </span>
 
         <div className="flex flex-col items-start gap-2">
           <h2 className="text-xl font-bold ">Growth Digital Marketing</h2>
-          <Separator />
+          {/*   <Separator className="bg-slate-700" /> */}
           <div className="flex flex-col gap-3">
             <h3 className="text-lg font-semibold">Benefícios</h3>
             <ul className="flex flex-col">
@@ -27,24 +27,110 @@ export function PriceCards() {
           </div>
         </div>
 
-        <div className="relative flex justify-between">
-          <span className="absolute right-0 top-0 flex items-center gap-2 rounded-lg border-slate-950 bg-transparent p-2 font-medium text-gray-100">
-            Desconto de 63%
-          </span>
-          <div className="flex flex-col gap-1">
-            <span>De</span>
-            <span className="line-through">R$ 15,99 /mês</span>
-          </div>
-          <div className="flex flex-col gap-1">
-            <span>Por</span>
-            <span>
-              <span className="text-xl font-bold">R$ 6 </span>
-              <span>/mês</span>
+        <div className="flex flex-col items-center gap-2">
+          <div className="relative flex w-full items-end justify-between pt-14">
+            <span className="w-25 h-15 absolute right-0 top-0 flex items-center gap-2 rounded-sm border  border-slate-100 bg-transparent p-2 text-sm font-medium text-gray-100">
+              Desconto de 63%
             </span>
+
+            <div className="flex flex-col">
+              <span className="font-medium">De</span>
+              <span className="flex ">
+                <span className="text-start">R$</span>
+                <span className="text-3xl font-bold line-through ">15,99</span>
+                <span className="mt-2">/mês</span>
+              </span>
+            </div>
+
+            <div className="flex flex-col">
+              <span className="font-medium">Por</span>
+              <span className="flex ">
+                <span className="text-start">R$</span>
+                <span className="text-5xl font-bold">6</span>
+                <span className="mt-5">/mês</span>
+              </span>
+            </div>
+          </div>
+          <Separator className="bg-slate-700" />
+          <span className="text-slate-400">pagamento anual - total R$ 72</span>
+        </div>
+
+        <Button className="bg-slate-100 text-base font-semibold text-primary hover:bg-slate-50/90">
+          Escolher
+        </Button>
+      </Card>
+
+      <Card className=" relative flex flex-col justify-between gap-12 bg-slate-50 p-6 pt-10 text-primary shadow-2xl">
+        <span className="absolute -top-4 left-4 flex items-center gap-2 rounded-sm bg-primary px-4 py-2 font-medium text-gray-100">
+          Básico
+        </span>
+
+        <div className="flex flex-col items-start gap-2">
+          <h2 className="text-xl font-bold ">Growth Digital Marketing</h2>
+          {/*   <Separator className="bg-slate-700" /> */}
+          <div className="flex flex-col gap-3">
+            <h3 className="text-lg font-semibold">Benefícios</h3>
+            <ul className="flex flex-col">
+              <li className="flex items-center gap-4">
+                <BsCheck2Circle className="text-blue1" /> Lorem ipsum dolor sit
+                amet consectetur
+              </li>
+            </ul>
           </div>
         </div>
 
-        <Button>Escolher</Button>
+        <div className="flex flex-col items-center gap-2">
+          <div className="relative flex w-full items-end justify-between pt-14">
+            <span className="text-2xl font-bold">
+              Sem <br />
+              Desconto
+            </span>
+            <div className="flex flex-col">
+              <span className="font-medium">Por</span>
+              <span className="flex ">
+                <span className="text-start">R$</span>
+                <span className="text-5xl font-bold">10,99</span>
+                <span className="mt-5">/mês</span>
+              </span>
+            </div>
+          </div>
+          <Separator className="bg-slate-700" />
+          <span className="text-slate-400">pagamento anual - total R$ 132</span>
+        </div>
+
+        <Button
+          variant="outline"
+          className="hover  bg-transparent text-base font-semibold text-primary"
+        >
+          Escolher
+        </Button>
+      </Card>
+
+      <Card className="relative flex flex-col justify-between gap-12 bg-slate-50 p-6 pt-10 text-primary shadow-2xl">
+        <span className="absolute -top-4 left-4 flex items-center gap-2 rounded-sm bg-primary px-4 py-2 font-medium text-gray-100">
+          Gratuito
+        </span>
+
+        <div className="flex flex-col items-start gap-2">
+          <h2 className="text-xl font-bold ">Growth Digital Marketing</h2>
+          {/*   <Separator className="bg-slate-700" /> */}
+          <div className="flex flex-col gap-3">
+            <h3 className="text-lg font-semibold">Benefícios</h3>
+            <ul className="flex flex-col">
+              <li className="flex items-center gap-4">
+                <BsCheck2Circle className="text-blue1" /> Lorem ipsum dolor sit
+                amet consectetur
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <Button
+          variant="outline"
+          className="hover  bg-transparent text-base font-semibold text-primary"
+        >
+          Escolher
+        </Button>
       </Card>
     </section>
   );
