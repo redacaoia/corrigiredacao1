f1e627223391017b7968602624eeb9694d2fa97c
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-02-12 21:15:03 +0000

    new

--- apps/nextjs/src/components/app/shared/appHeader/fragments/headerContent.tsx
+++ apps/nextjs/src/components/app/shared/appHeader/fragments/headerContent.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { GreetingByTime } from "~/utils/greetingByTime";
+//import { GreetingByTime } from "~/utils/greetingByTime";
 
 interface HeaderContentProps {
   name: string;
 }
 
 export async function HeaderContent({ name }: HeaderContentProps) {
-  const greetingByTime = GreetingByTime();
+  /* const greetingByTime = GreetingByTime(); */
 
   return (
     <div className="flex flex-col gap-1 text-white">
-      <h2 className="text-xl font-bold">{greetingByTime}👋</h2>
-      <h3 className="text-base font-normal">Bem Vindo, {name}</h3>
+      <h2 className="text-xl font-bold">Olá👋</h2>
+      <h3 className="text-base font-normal">{name}</h3>
     </div>
   );
 }
