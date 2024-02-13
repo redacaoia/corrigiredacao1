35d7dcd5d2951992af53b404d5b534cdf27a79e4
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-02-13 13:32:51 +0000

    new

--- apps/nextjs/src/components/app/shared/BottomMenu/fragments/BottomMenuIcon.tsx
+++ apps/nextjs/src/components/app/shared/BottomMenu/fragments/BottomMenuIcon.tsx
@@ -2,6 +2,7 @@
 
 import type { LinkProps } from "next/link";
 import type { ElementType } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -9,7 +10,7 @@ type BottomMenuIconProps = LinkProps & {
   icon: ElementType;
   iconActive: ElementType;
   color?: string;
-  imageUrl?: string
+  imageUrl?: string;
   href: string;
 };
 
@@ -27,14 +28,17 @@ export function BottomMenuIcon({
       href={href}
       className="cursor-pointer rounded-full p-2 transition-all duration-300 hover:bg-slate-100 hover:transition-all hover:duration-300"
     >
-      {pathname === href ? (
+      {pathname === href && !imageUrl ? (
         <IconActive size="35" color="#0157C8" />
       ) : (
         <Icon size="35" color="#222222" />
       )}
-      {
-        
-      }
+
+      {pathname === href && imageUrl ? (
+        <Image src="/icons/desempenho1.svg" className="text-blue1" alt="" />
+      ) : (
+        <Image src="/icons/desempenho.svg" className="text-primary" alt="" />
+      )}
     </Link>
   );
 }
