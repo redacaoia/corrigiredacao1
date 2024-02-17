6d0a7cc1e2f09ff79a02b87b73f04b37e9822f2f
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-02-17 22:42:10 +0000

    new

--- apps/nextjs/src/components/marketing/shared/auth/spotifyButton.tsx
+++ apps/nextjs/src/components/marketing/shared/auth/spotifyButton.tsx
@@ -1,29 +1,62 @@
+"use client";
+
+import type { ElementType } from "react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import { useSessionContext } from "@supabase/auth-helpers-react";
 import { FcGoogle } from "react-icons/fc";
 
+import LoadingDots from "../icons/loadingDots";
+
 type Props = {
   type?: "signin" | "signup";
+  provider: any;
+  icon: ElementType;
 };
 
-export const GoogleButton: React.FC<Props> = ({ type }) => {
+export const ButtonSignIn: React.FC<Props> = ({
+  type,
+  provider,
+  icon: Icon,
+}) => {
+  const [signInClicked, setSignInClicked] = useState(false);
+
   const { supabaseClient } = useSessionContext();
 
-  const handleGoogleSignUp = async () => {
+  const handleSignIn = async () => {
     const { data, error } = await supabaseClient.auth.signInWithOAuth({
-      provider: "spotify",
+      provider: provider,
     });
   };
 
   return (
-    <button
-      className="border-1 flex w-full justify-center rounded-md border border-gray-300 px-4 py-2 text-sm font-medium text-gray-900 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+    <Button
+      disabled={signInClicked}
+      className={`${
+        signInClicked
+          ? "cursor-not-allowed border-gray-200 bg-gray-100"
+          : "border border-gray-200 bg-white text-black hover:bg-gray-50"
+      } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
       type="button"
-      onClick={handleGoogleSignUp}
+      onClick={() => {
+        handleSignIn;
+        setSignInClicked(true);
+      }}
     >
-      <div className="flex gap-2">
-        <FcGoogle className="w-4" />
-        {type === "signup" ? "Criar com Google" : "Login com Google"}
-      </div>
-    </button>
+      {signInClicked ? (
+        <LoadingDots color="#808080" />
+      ) : (
+        <>
+          <div className="flex gap-2">
+            <Icon className="w-4" />
+            <p>
+              {type === "signup"
+                ? `Criar com ${provider}`
+                : `Entrar com ${provider}`}
+            </p>
+          </div>
+        </>
+      )}
+    </Button>
   );
 };
