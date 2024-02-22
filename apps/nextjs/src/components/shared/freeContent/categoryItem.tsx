b06eb40c2bbce5143cceae0c7e3f1a4c9501231a
redacaoia <157169581+redacaoia@users.noreply.github.com>
2024-02-22 22:15:14 +0000

    new

--- apps/nextjs/src/components/shared/freeContent/categoryItem.tsx
+++ apps/nextjs/src/components/shared/freeContent/categoryItem.tsx
@@ -2,14 +2,14 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { CarouselItem  } from "@/components/ui/carousel";
 import { CategoryItem } from "@/types/freeContent";
 import Link from "next/link";
-
+//basis-48
 export function CategoryItem({description,href,title}:CategoryItem) {
   return (
-    <CarouselItem key={title} className="pl-1 basis-48 md:basis-1/2 lg:basis-1/3">
+    <CarouselItem key={title} className="pl-1 basis-48  md:basis-1/2 lg:basis-1/3">
     <div className="p-1">
         <Link href={href}>
         
-        <Card className="w-48">
+        <Card className="h-60 md:auto md:w-auto">
         <CardHeader>
     <CardTitle>{title}</CardTitle>
     <CardDescription>{description}</CardDescription>
