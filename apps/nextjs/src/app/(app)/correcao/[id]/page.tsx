6b083acc0bb7fc04f563a55eef873544eda3c78a
ticket-generate-score <157168428+ticket-generate-score@users.noreply.github.com>
2024-02-28 17:07:51 +0000

    new

---
+++ apps/nextjs/src/app/(app)/correcao/[id]/page.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function Correction({ params }: Props) {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="w-full max-w-screen-sm rounded-lg bg-white p-8 shadow-md">
+        <h1 className="mb-4 text-2xl font-bold">Hello, Instagram!</h1>
+
+        <p className="text-gray-600">This is my awesome story content.</p>
+      </div>
+    </div>
+  );
+}
---
+++ apps/nextjs/src/app/(marketing)/termos-de-servico/page.tsx
@@ -0,0 +1,5 @@
+export default function TermsOfUse() {
+    return(
+      <h1> Planos</h1>
+    )
+  }
