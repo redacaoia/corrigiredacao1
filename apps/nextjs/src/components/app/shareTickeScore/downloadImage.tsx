ced64ef2c30e61956b5134df4c60e5e55d9d5158
ticket-generate-score <157168428+ticket-generate-score@users.noreply.github.com>
2024-02-28 21:50:24 +0000

    new

--- apps/nextjs/src/components/app/shareTickeScore/downloadImage.tsx
+++ apps/nextjs/src/components/app/shareTickeScore/downloadImage.tsx
@@ -6,7 +6,7 @@ export function DownloadImage() {
   const handleClick = async () => {
     const response = await fetch("/api/file/score-ticket-image/");
     const blob = await response.blob();
-    const url = window.URL.createObjectURL(blob);
+    const url = await fetch("https://og-examples.vercel.sh/api/dynamic-image?username=vercel");
     const link = document.createElement("a");
     link.href = url;
     link.download = "image.png";
