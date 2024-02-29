637b4bb8c6777ce57ecb9ad6815b75199709f1b6
ticket-generate-score <157168428+ticket-generate-score@users.noreply.github.com>
2024-02-29 10:59:25 +0000

    new

--- apps/nextjs/src/components/app/cancelSubscription/multiStepFform/index.tsx
+++ apps/nextjs/src/components/app/cancelSubscription/multiStepFform/index.tsx
@@ -142,14 +142,18 @@ export function MultiStepForm() {
     if (Object.values(errors).some((error) => error)) {
       return;
     }
-    toast("Event has been created", {
+    
+    nextStep();
+
+    const {addOns,email,name,phone,plan,yearly} = formData
+
+    toast(`${email} ${name} ${email} ${phone} ${plan} ${yearly}`, {
       description: "Sunday, December 03, 2023 at 9:00 AM",
       action: {
         label: "Undo",
         onClick: () => console.log("Undo"),
       },
     });
-    nextStep();
   };
 
   return (
