14eef775d38516ef7af94899116e6f8f460eefb5
ticket-generate-score <157168428+ticket-generate-score@users.noreply.github.com>
2024-03-01 22:28:44 +0000

    new

--- apps/nextjs/src/app/(app)/correcao/[id]/page.tsx
+++ apps/nextjs/src/app/(app)/correcao/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { ConfettiContainer } from "@/components/app/correction/confetti";
+import {Confetti} from "@/components/app/correction/confetti";
 import { ShareTickeScore } from "@/components/app/shareTickeScore";
 
 interface Props {
@@ -9,14 +9,15 @@ interface Props {
 
 export default function Correction({ params }: Props) {
   return (
-    <ConfettiContainer>
+   
       <div className="flex h-screen items-center justify-center bg-gray-100">
+        <Confetti/>
         <div className="w-full max-w-screen-sm rounded-lg bg-white p-8 shadow-md">
           <h1 className="mb-4 text-2xl font-bold">Hello, Instagram!</h1>
         {/*   <ShareTickeScore imageUrl="" username="" score=""  /> */}
           <p className="text-gray-600">This is my awesome story content.</p>
         </div>
       </div>
-    </ConfettiContainer>
+    
   );
 }
--- apps/nextjs/src/components/app/correction/confetti.tsx
+++ apps/nextjs/src/components/app/correction/confetti.tsx
@@ -1,59 +1,69 @@
 "use client";
 
-import { useEffect } from "react";
-import Confetti from "react-canvas-confetti";
+import { useCallback, useEffect, useRef } from "react";
+import ReactCanvasConfetti from "react-canvas-confetti";
 
 interface Props {
   children: React.ReactNode;
 }
-export function ConfettiContainer({ children }: Props) {
-  useEffect(() => {
-    // Adicione a lógica para disparar os confetes ao carregar a página
-    const duration = 15 * 1000;
-    const animationEnd = Date.now() + duration;
-    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+export function Confetti() {
+  const refAnimationInstance = useRef(null);
 
-    function randomInRange(min, max) {
-      return Math.random() * (max - min) + min;
-    }
-
-    function createConfettiParticle() {
-      const angle = randomInRange(0, 360);
-      const velocity = randomInRange(10, 20);
-      const confettiProps = {
-        ...defaults,
-        particleCount: 1,
-        angle,
-        spread: randomInRange(20, 80),
-        startVelocity: randomInRange(5, 10),
-        colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
-      };
-
-      return (
-        <Confetti
-          {...confettiProps}
-          key={Date.now() + randomInRange(0, 10000)}
-        />
-      );
-    }
-
-    function createConfettiBurst() {
-      const timeLeft = animationEnd - Date.now();
-
-      if (timeLeft <= 0) {
-        return;
-      }
-
-      const burstCount = Math.floor((timeLeft / 1000) * 50);
-      for (let i = 0; i < burstCount; i++) {
-        setTimeout(createConfettiParticle, i * 500);
-      }
-
-      setTimeout(createConfettiBurst, 500);
-    }
-
-    createConfettiBurst();
+  const getInstance = useCallback((instance) => {
+    refAnimationInstance.current = instance;
   }, []);
 
-  return <div> {children}</div>;
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio),
+      });
+  }, []);
+
+  useEffect(() => fire(), []);
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    makeShot(0.2, {
+      spread: 60,
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  }, [makeShot]);
+
+  return (
+    <ReactCanvasConfetti
+      refConfetti={getInstance}
+      style={{
+        position: "fixed",
+        pointerEvents: "none",
+        width: "100%",
+        height: "100%",
+        top: 0,
+        left: 0,
+      }}
+    />
+  );
 }
