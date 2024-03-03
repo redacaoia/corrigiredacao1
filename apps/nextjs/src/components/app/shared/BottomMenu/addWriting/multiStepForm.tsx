34b895d51d7e9f8c371b4cb46e4e84c675934f2c
ticket-generate-score <157168428+ticket-generate-score@users.noreply.github.com>
2024-03-03 13:44:55 +0000

    new

--- apps/nextjs/src/components/app/shared/BottomMenu/addWriting/multiStepForm.tsx
+++ apps/nextjs/src/components/app/shared/BottomMenu/addWriting/multiStepForm.tsx
@@ -102,7 +102,7 @@ export function MultiStepForm() {
             className="space-y-4 md:flex md:space-x-8 md:space-y-0"
           >
             {steps.map((step, index) => (
-              <li key={step.name} className="md:flex-1">
+              <li key={step.name} className="flex-1">
                 {currentStep > index ? (
                   <div className="group flex w-full flex-col border-l-4 border-sky-600 py-2 pl-4 transition-colors md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4">
                     <span className="text-sm font-medium text-sky-600 transition-colors ">
