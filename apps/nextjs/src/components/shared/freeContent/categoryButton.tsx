dfd17eeb57b75c93a0fc5f2e45b9c6a8d5adb681
ticket-generate-score <157168428+ticket-generate-score@users.noreply.github.com>
2024-03-04 17:56:43 +0000

    new

--- apps/nextjs/src/components/shared/freeContent/categoryButton.tsx
+++ apps/nextjs/src/components/shared/freeContent/categoryButton.tsx
@@ -1,6 +1,6 @@
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { FaArrowRight } from "react-icons/fa";
-import { Link as ScrollLink } from "react-scroll";
 
 interface Props {
   categoryTitle: string;
@@ -11,25 +11,17 @@ export function CategoryButton({ categoryTitle }: Props) {
   const withoutAccents = categoryTitle
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
-
   // Substitui espaços, hifens e pontos por nada
   const formattedTitle = withoutAccents.replace(/[ -./]/g, "");
-
   // Converte para minúsculas
   const lowercaseTitle = formattedTitle.toLowerCase();
 
   return (
-    <div>
-      {/*   <ScrollLink
-        to={lowercaseTitle} // ID da seção para a qual você deseja rolar
-        smooth={true}
-        duration={500} // duração da animação em milissegundos
-      > */}
+    <Link href={lowercaseTitle}>
       <Button className="flex h-16 w-48 items-center gap-2 rounded-sm bg-blue-500 text-center text-lg font-medium shadow-sm hover:bg-sky-600 md:h-24">
         {categoryTitle} <FaArrowRight className="hover:animate-bellRing" />
       </Button>
-      {/*  </ScrollLink> */}
-    </div>
+    </Link>
   );
 }
 
--- apps/nextjs/src/config/assessments.ts
+++ apps/nextjs/src/config/assessments.ts
@@ -1,29 +1,28 @@
 import { AssessmentsDataPros } from "~/types/site";
 
-
 export const AssessmentsData: AssessmentsDataPros[] = [
-    {
-        videoUrl: "",
-        name: 'Luciana Oliveira',
-        rating: 5,
-        comment: 'A redação IA revolucionou minha preparação para o Enem!',
-        description:
-            'Recebi feedbacks detalhados que destacaram meus pontos fracos e sugeriram melhorias específicas. Isso foi crucial para minha evolução como escritor.',
-    },
-    {
-        videoUrl: "",
-        name: 'Rafael Silva',
-        rating: 4,
-        comment: 'O feedback detalhado me ajudou a entender meus pontos fracos.',
-        description:
-            'Recebi feedbacks detalhados que destacaram meus pontos fracos e sugeriram melhorias específicas. Isso foi crucial para minha evolução como escritor.',
-    },
-    {
-        videoUrl: "",
-        name: 'Mariana Santos',
-        rating: 5,
-        comment: 'Incrível como a IA compreende nuances e nuances linguísticas!',
-        description:
-            'A redação IA é impressionante!Além disso, a rapidez na correção me permitiu otimizar meu tempo de estudo. Estou verdadeiramente satisfeita e confiante para enfrentar a redação do Enem.',
-    },
-]
+  {
+    videoUrl: "",
+    name: "Luciana Oliveira",
+    rating: 5,
+    comment: "A redação IA revolucionou minha preparação para o Enem!",
+    description:
+      "Recebi feedbacks detalhados que destacaram meus pontos fracos e sugeriram melhorias específicas. Isso foi crucial para minha evolução como escritor.",
+  },
+  {
+    videoUrl: "",
+    name: "Rafael Silva",
+    rating: 4,
+    comment: "O feedback detalhado me ajudou a entender meus pontos fracos.",
+    description:
+      "Recebi feedbacks detalhados que destacaram meus pontos fracos e sugeriram melhorias específicas. Isso foi crucial para minha evolução como escritor.",
+  },
+  {
+    videoUrl: "",
+    name: "Mariana Santos",
+    rating: 5,
+    comment: "Incrível como a IA compreende nuances e nuances linguísticas!",
+    description:
+      "A redação IA é impressionante!Além disso, a rapidez na correção me permitiu otimizar meu tempo de estudo. Estou verdadeiramente satisfeita e confiante para enfrentar a redação do Enem.",
+  },
+];
