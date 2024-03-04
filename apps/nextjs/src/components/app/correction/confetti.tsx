3aebc55624aabeb5f4d73724cc503bec3eb2be7b
ticket-generate-score <157168428+ticket-generate-score@users.noreply.github.com>
2024-03-04 18:12:03 +0000

    new

--- apps/nextjs/src/components/app/correction/confetti.tsx
+++ apps/nextjs/src/components/app/correction/confetti.tsx
@@ -1,73 +1,33 @@
 "use client";
 
-import { useCallback, useEffect, useRef } from "react";
-import ReactCanvasConfetti from "react-canvas-confetti";
+import { useCallback, useEffect, useRef, useState } from "react";
+import Reactconfetti, { ReactConfetti } from "react-confetti";
 
 interface Props {
   children: React.ReactNode;
 }
 export function Confetti({ children }: Props) {
-  const refAnimationInstance = useRef(null);
-  let bb;
-  const getInstance = useCallback((instance) => {
-    refAnimationInstance.current = instance;
-  }, []);
+  const [windowDimension, setDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
-  const makeShot = useCallback((particleRatio, opts) => {
-    refAnimationInstance.current &&
-      refAnimationInstance.current({
-        ...opts,
-        origin: { y: 0.7 },
-        particleCount: Math.floor(200 * particleRatio),
-      });
-  }, []);
+  const detectSize = () => {
+    setDimension({ width: window.innerWidth, height: window.innerHeight });
+  };
 
-  useEffect(() => fire(), []);
-
-  const fire = useCallback(() => {
-    makeShot(0.25, {
-      spread: 26,
-      startVelocity: 55,
-    });
-
-    makeShot(0.2, {
-      spread: 60,
-    });
-
-    makeShot(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8,
-    });
-
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2,
-    });
-
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 45,
-    });
-  }, [makeShot]);
+  useEffect(() => {
+    window.addEventListener("resize", detectSize);
+    return () => {
+      window.removeEventListener("resize", detectSize);
+    };
+  }, [windowDimension]);
 
   return (
     <>
-      <ReactCanvasConfetti
-       refConfetti={getInstance}
-        onInit={bb}
-        width={100}
-        height={100}
-        style={{
-          position: "fixed",
-          pointerEvents: "none",
-          width: "100%",
-          height: "100%",
-          top: 0,
-          left: 0,
-        }}
+      <ReactConfetti
+        width={windowDimension.width}
+        height={windowDimension.height}
       />
       {children}
     </>
